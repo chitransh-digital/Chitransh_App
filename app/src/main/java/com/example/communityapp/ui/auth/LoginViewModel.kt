bfd852b889b8a4403b51362e0eba569a3703d8aa
AgPriyanshu18 <agpriyanshu18@gmail.com>
2023-12-24 22:47:42 +0530

    Phone Verification Done

--- app/src/main/java/com/example/communityapp/ui/auth/LoginViewModel.kt
+++ app/src/main/java/com/example/communityapp/ui/auth/LoginViewModel.kt
@@ -32,39 +32,25 @@ class LoginViewModel@Inject constructor(private val auth: FirebaseAuth) : ViewMo
         val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
             override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-                // This callback will be invoked in two situations:
-                // 1 - Instant verification. In some cases the phone number can be instantly
-                //     verified without needing to send or enter a verification code.
-                // 2 - Auto-retrieval. On some devices Google Play services can automatically
-                //     detect the incoming verification SMS and perform verification without
-                //     user action.
                 verification.value = Resource.success(credential.toString())
                 Log.d(TAG, "onVerificationCompleted:$credential")
                 signInWithPhoneAuthCredential(credential,activity)
             }
 
             override fun onVerificationFailed(e: FirebaseException) {
-                // This callback is invoked in an invalid request for verification is made,
-                // for instance if the the phone number format is not valid.
                 Log.w(TAG, "onVerificationFailed", e)
 
                 if (e is Exception) {
                     verification.value = Resource.error(e)
                 }
-
-                // Show a message and update the UI
             }
 
             override fun onCodeSent(
                 verificationId: String,
                 token: PhoneAuthProvider.ForceResendingToken,
             ) {
-                // The SMS verification code has been sent to the provided phone number, we
-                // now need to ask the user to enter the code and then construct a credential
-                // by combining the code with a verification ID.
                 Log.d(TAG, "onCodeSent:$verificationId")
-
-                // Save verification ID and resending token so we can use them later
+                verification.value = Resource.success(verificationId)
                 Constants.verID = verificationId
                 Constants.token = token.toString()
             }
@@ -82,21 +68,17 @@ class LoginViewModel@Inject constructor(private val auth: FirebaseAuth) : ViewMo
 
     }
 
-    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential, activity: Activity) {
+    fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential, activity: Activity) {
         auth.signInWithCredential(credential)
             .addOnCompleteListener(activity) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithCredential:success")
 
                     val user = task.result?.user
                 } else {
-                    // Sign in failed, display a message and update the UI
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
                     if (task.exception is FirebaseAuthInvalidCredentialsException) {
-                        // The verification code entered was invalid
                     }
-                    // Update UI
                 }
             }
     }
--- app/src/main/java/com/example/communityapp/ui/auth/Login_activity.kt
+++ app/src/main/java/com/example/communityapp/ui/auth/Login_activity.kt
@@ -3,6 +3,7 @@ package com.example.communityapp.ui.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -10,6 +11,7 @@ import com.example.communityapp.R
 import com.example.communityapp.databinding.ActivityLoginBinding
 import com.example.communityapp.utils.Constants
 import com.example.communityapp.utils.Resource
+import com.google.firebase.auth.PhoneAuthProvider
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,6 +19,7 @@ class Login_activity : AppCompatActivity() {
 
     private lateinit var binding : ActivityLoginBinding
     private lateinit var viewModel: LoginViewModel
+    private lateinit var verificationID : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -26,10 +29,20 @@ class Login_activity : AppCompatActivity() {
 
         binding.button.setOnClickListener {
             val ph = "+91" + binding.editTextPhone.text.toString()
-            if(ph.isNullOrEmpty()){
+            if(ph.isEmpty()){
                 Toast.makeText(this, "Input your phone number", Toast.LENGTH_SHORT).show()
             }else{
                 viewModel.OnVerificationCodeSent(ph,this)
+            }
+        }
+
+        binding.buttonotp.setOnClickListener {
+            val otp = binding.editTextPhone.text.toString()
+            if(otp.isEmpty()){
+                Toast.makeText(this, "Please enter otp", Toast.LENGTH_SHORT).show()
+            }else{
+                val credential = PhoneAuthProvider.getCredential(verificationID, otp)
+                viewModel.signInWithPhoneAuthCredential(credential,this)
             }
         }
 
@@ -40,12 +53,14 @@ class Login_activity : AppCompatActivity() {
             when(resouce.status){
                 Resource.Status.SUCCESS->{
                     Log.e("url",resouce.status.toString())
+                    codesent(resouce.data!!)
                 }
                 Resource.Status.ERROR->{
                     Log.e("url",resouce.status.toString())
                 }
                 Resource.Status.LOADING->{
                     Log.e("url","loading")
+
                 }
                 else -> {
                     Log.e("url","else")
@@ -56,5 +71,13 @@ class Login_activity : AppCompatActivity() {
 
 
 
+    }
+
+    private fun codesent(data: String) {
+        verificationID = data
+        binding.text.text = "Please enter the OTP"
+        binding.editTextPhone.text.clear()
+        binding.button.visibility = View.GONE
+        binding.buttonotp.visibility = View.VISIBLE
     }
 }
